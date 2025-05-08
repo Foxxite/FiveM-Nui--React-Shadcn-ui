0046789c42ee53c5da725fb1234d9cc8ba0698c2
Foxxite <foxxite@foxxite.com>
2025-05-08 14:46:45 +0200

    broken

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { browserslistToTargets } from 'lightningcss';
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
                 plugins: [['babel-plugin-react-compiler', { target: '19' }]],
             },
         }),
+        tailwindcss(),
     ],
     resolve: {
         alias: {
